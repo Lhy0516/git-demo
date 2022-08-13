b71e8a84f7bca11ab30b722115887b53f28a244a
Lhy <Lhy@qq.com>
2022-08-13 13:30:24 +0800

    Mybatis-plus first commit

---
+++ src/test/java/com/lhy/demo_mybatisplus/DemoMybatisPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhy.demo_mybatisplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMybatisPlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
